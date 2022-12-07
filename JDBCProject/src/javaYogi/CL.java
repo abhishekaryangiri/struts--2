b21c32630e710790f1b0d2c8e52c794a85ba12d4
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-07 20:34:01 +0530

    dynamic

--- JDBCProject/src/javaYogi/CL.java
+++ JDBCProject/src/javaYogi/CL.java
@@ -1,14 +1,23 @@
 package javaYogi;
 
+import java.util.Scanner;
+
 public class CL {
 public static void main(String[] args) {
-	int uid=12;
-	String name="Ramdyalu";
-	String add="Greater noida";
-	String phone="8888330890";
+
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter EId");
+	int uid = Integer.parseInt(scan.nextLine());
+	System.out.println("Enter Name");
+	String n = scan.nextLine();
+	System.out.println("Enter Add");
+	String ad = scan.nextLine();
+	System.out.println("Enter Mobile Number");
+	String p = scan.nextLine();
+	
 
 	UserDAO userDAO = new UserDAO();
-	if(userDAO.insert(uid, name, add, phone))
+	if(userDAO.insert(uid, n, ad, p))
 	{
 		System.out.println("successfull");
 	}
