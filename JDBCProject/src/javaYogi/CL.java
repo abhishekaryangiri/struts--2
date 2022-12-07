b062702b9d6e66cc9af970d6e25df1aa687361b9
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-07 20:12:41 +0530

    successfull

--- JDBCProject/src/javaYogi/CL.java
+++ JDBCProject/src/javaYogi/CL.java
@@ -2,10 +2,10 @@ package javaYogi;
 
 public class CL {
 public static void main(String[] args) {
-	int uid=11;
-	String name="Shankram";
-	String add="noida";
-	String phone="8826930890";
+	int uid=12;
+	String name="Ramdyalu";
+	String add="Greater noida";
+	String phone="8888330890";
 
 	UserDAO userDAO = new UserDAO();
 	if(userDAO.insert(uid, name, add, phone))
--- JDBCProject/src/javaYogi/UserDAO.java
+++ JDBCProject/src/javaYogi/UserDAO.java
@@ -2,6 +2,7 @@ package javaYogi;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class UserDAO {
 	
@@ -10,12 +11,22 @@ public class UserDAO {
 		try {//java se db me jane p try catch lgana padega
 			Class.forName("org.postgresql.Driver");  //step1
 			String url="jdbc:postgresql://localhost:5432/26Sep";
-			Connection con = DriverManager.getConnection(url, "postgres", "postgres");
+			Connection con = DriverManager.getConnection(url, "postgres", "postgres"); //step2
 			//connection interface is a factory of statement,prepared statement, and callable statement
 			//statement: 1. jaha static query fire krna ho, 2. query everytime compile and exicutre hoti h, 3. Insecure hota h( bcz koe v sql injection de skta h)
 			//prepared stat: 1. jaha parametrise(dynamic) fire krna ho, 2.  query first time compile hoga second time exicute exicuting ( pre compiled) hota h, 3.no sql injection
 			
-			con.prepareStatement("insert into emp values( ?, ?, ?, ?");
+			PreparedStatement ps = con.prepareStatement("insert into emp values( ?, ?, ?, ?)");//step3
+			ps.setInt(1, uid);
+			ps.setString(2, name);
+			ps.setString(3, add);
+			ps.setString(4, phone);
+			
+			int i = ps.executeUpdate(); //step4
+			if(i>0) 
+			{
+				return true;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
